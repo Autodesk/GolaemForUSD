2e7f23d073cf9f79d0f05784dd6cf00bdfe1ed7d
Daniel Trusca <daniel.trusca@autodesk.com>
2025-04-18 13:36:32 +0200

    Reports from Adsk repo

--- src/version/glmUSDPluginVersion.h
+++ src/version/glmUSDPluginVersion.h
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include <glmCoreVersion.h>
+
 //description of the component
 #define GLM_USDPLUGIN_NAME "GolaemUSDPlugin"
-#define GLM_USDPLUGIN_DESCRIPTION "Golaem USD Plugin. Copyright (C) Golaem S.A.  All Rights Reserved."
+#define GLM_USDPLUGIN_DESCRIPTION "Autodesk Golaem USD Plugin"
